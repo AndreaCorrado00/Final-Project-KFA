9461a0a0b0b2d74c6aa526d8ef3ac0d5904e131b
AndreaCorrado00 <corradoandrea00@gmail.com>
2023-05-17 09:17:02 +0200

    Recipes database

--- my_project/lib/Database/Advice_Database.dart
+++ my_project/lib/Database/Advice_Database.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-Map<int, dynamic> Advices={1: adv1,2: adv2,3: adv3,4: adv4,5: adv5,6: adv6,7: adv7,8: adv8,9:adv9,10:adv10,11:adv11,12:adv12,13:adv13,14:adv14,15:adv15,16:adv16,17:adv17};
+// DAILY ADVICES
+Map<int, dynamic> Advices={0: adv1,1: adv2,2: adv3,3: adv4,4: adv5,5: adv6,6: adv7,7: adv8,8:adv9,9:adv10,10:adv11,11:adv12,12:adv13,13:adv14,14:adv15,15:adv16,16:adv17};
 Map<String, String> adv1={
   'adv_text': 'The impact of the COVID-19 pandemic reversed the steady progress of poverty reduction over the past 25 years. ',
   'url':'https://www.un.org/sustainabledevelopment/poverty/'};
@@ -70,10 +71,251 @@ Map<String, String> adv17={
   'url':'https://www.un.org/sustainabledevelopment/globalpartnerships/'};
 
 
-//final lenght_adv=Advices.length;
+// DAILY RECIPES
+
+Map<int, dynamic> Recipes={0: Rep1,1: Rep2,2: Rep3,3: Rep4,4: Rep5,5: Rep6,6: Rep7,7: Rep8, 8:Rep9, 9:Rep10,10:Rep11,11:Rep12,12:Rep13,13:Rep14,14:Rep15,
+                          15:Rep16,16:Rep17,17:Rep18,18:Rep19,19:Rep20,20:Rep21,21:Rep22,22:Rep23,23:Rep24,24:Rep25,25:Rep26,26:Rep27,27:Rep28,28:Rep29,29:Rep30};
+
+Map<String, String> Rep1={
+  'adv_text': 'Filo-wrapped hake with saffron yogurt',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/filo-wrapped-hake-with-saffron-yogurt/'};
+
+Map<String, String> Rep2={
+  'adv_text': 'Peppercorn mushrooms with butter bean mash',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/peppercorn-mushrooms-with-butter-bean-mash/'};
+
+Map<String, String> Rep3={
+  'adv_text': 'Sea bream with spring onion and brown butter rice',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/sea-bream-with-spring-onion-and-brown-butter-rice/'};
+
+Map<String, String> Rep4={
+  'adv_text': 'Turmeric-roasted hake with coconut sauce',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/turmeric-roasted-hake-with-coconut-sauce/'};
+
+Map<String, String> Rep5={
+  'adv_text': 'Speedy poached pollock with aromatic spices',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/speedy-poached-pollock-with-aromatic-spices/'};
+
+Map<String, String> Rep6={
+  'adv_text': 'Pressure cooker refried beans',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/pressure-cooker-refried-beans/'};
+
+Map<String, String> Rep7={
+  'adv_text': 'Ultimate venison burgers',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/ultimate-venison-burgers/'};
+
+Map<String, String> Rep8={
+  'adv_text': 'Coffee-marinated roast venison saddle',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/coffee-marinated-roast-venison-saddle/'};
+
+Map<String, String> Rep9={
+  'adv_text': 'Black bean chilli soup with corn dumplings',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/black-bean-chilli-soup-with-corn-dumplings/'};
+
+Map<String, String> Rep10={
+  'adv_text': 'Grilled mussels with garlic and parsley',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/grilled-mussels-with-garlic-and-parsley/'};
+
+Map<String, String> Rep11={
+  'adv_text': 'Vegan cabbage cacciatore',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/vegan-cabbage-cacciatore/'};
+
+Map<String, String> Rep12={
+  'adv_text': 'Vegan butternut squash risotto with millet',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/vegan-butternut-squash-risotto-with-millet/'};
+
+Map<String, String> Rep13={
+  'adv_text': 'Vegan butternut squash risotto with millet',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/vegan-butternut-squash-risotto-with-millet/'};
+
+Map<String, String> Rep14={
+  'adv_text': 'Brussels sprout stir-fry with paneer and coconut',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/brussels-sprout-stir-fry-with-paneer-and-coconut/'};
+
+Map<String, String> Rep15={
+  'adv_text': 'Leftover roast potato breakfast hash',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/leftover-roast-potato-breakfast-hash/'};
+
+Map<String, String> Rep16={
+  'adv_text': 'Mackerel, watercress and mustard fishcakes',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/mackerel-watercress-and-mustard-fishcakes/'};
+
+Map<String, String> Rep17={
+  'adv_text': 'Runner bean and apple chutney',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/runner-bean-and-apple-chutney/'};
+
+  Map<String, String> Rep18={
+  'adv_text': 'Venison with pumpkin mash and cavolo nero',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/venison-with-pumpkin-mash-and-cavolo-nero/'};
+
+Map<String, String> Rep19={
+  'adv_text': 'Prebranac (caramelised-onion baked beans)',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/prebranac-caramelised-onion-baked-beans/'};
+
+Map<String, String> Rep20={
+  'adv_text': 'Mussels and clams with beans and sobrasada',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/mussels-and-clams-with-beans-and-sobrasada/'};
+
+Map<String, String> Rep21={
+  'adv_text': 'Veggie bean casserole with pesto garlic croûtes',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/veggie-bean-casserole-with-pesto-garlic-croutes/'};
+
+Map<String, String> Rep22={
+  'adv_text': 'Fish kievs',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/fish-kievs/'};
+
+Map<String, String> Rep23={
+  'adv_text': 'Root vegetable and lentil tagine',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/root-vegetable-and-lentil-tagine/'};
+
+Map<String, String> Rep24={
+  'adv_text': 'Grilled mackerel with apple and radish salad',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/grilled-mackerel-apple-radish-salad/'};
+
+Map<String, String> Rep25={
+  'adv_text': 'Veggie hash with fried eggs',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/veggie-hash-with-fried-eggs/'};
+
+Map<String, String> Rep26={
+  'adv_text': 'Saffron chicken stew with greens and chickpeas',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/saffron-chicken-stew-with-greens-and-chickpeas/'};
+
+Map<String, String> Rep27={
+  'adv_text': 'Strawberry and elderflower sorbet',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/strawberry-elderflower-sorbet/'};
+
+Map<String, String> Rep28={
+  'adv_text': 'Harissa-spiced butter beans with peppers and feta',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/harissa-butter-beans-peppers-feta/'};
   
+Map<String, String> Rep29={
+  'adv_text': 'Bramble and bay jam',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/bramble-and-bay-jam/'};
+
+Map<String, String> Rep30={
+  'adv_text': 'Vegan chilli with rice',
+  'url':'https://www.deliciousmagazine.co.uk/recipes/vegan-chilli-with-rice/'};
 
 
+  Map<int, dynamic> Curiosities={0: Cur1,1: Cur2,2: Cur3,3: Cur4,4: Cur5,5: Cur6,6: Cur7,7: Cur8, 8:Cur9, 9:Cur10,10:Cur11,11:Cur12,12:Cur13,13:Cur14,14:Cur15,
+                          15:Cur16,16:Cur17,17:Cur18,18:Cur19,19:Cur20,20:Cur21,21:Cur22,22:Cur23,23:Cur24,24:Cur25,25:Cur26,26:Cur27,27:Cur28,28:Cur29,29:Cur30};
 
+Map<String, String> Cur1={
+  'adv_text': '',
+  'url':''};
 
+Map<String, String> Cur2={
+  'adv_text': '',
+  'url':''};
 
+Map<String, String> Cur3={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur4={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur5={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur6={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur7={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur8={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur9={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur10={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur11={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur12={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur13={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur14={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur15={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur16={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur17={
+  'adv_text': '',
+  'url':''};
+
+  Map<String, String> Cur18={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur19={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur20={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur21={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur22={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur23={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur24={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur25={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur26={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur27={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur28={
+  'adv_text': '',
+  'url':''};
+  
+Map<String, String> Cur29={
+  'adv_text': '',
+  'url':''};
+
+Map<String, String> Cur30={
+  'adv_text': '',
+  'url':''};
